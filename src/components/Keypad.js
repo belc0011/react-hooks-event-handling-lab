0813cf6f1c33a3218e74ce1b69abed303374e2ef
Monica Garcia <monicairenegarcia@gmail.com>
2024-01-15 09:04:33 -0700

    completed event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,14 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad () {
+    function handleOnChange(event) {
+        console.log('Entering password...')
+        return event.target.value
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleOnChange}></input>
+        </div>
     )
 }
 
